beefa73bf38decaf14e3a90276acee42ef4d7709
Gilbert115 <Gilbertbriones402@RUSDlearns.net>
2015-10-02 14:40:42 -0700

    Create forloopprogram.vb

---
+++ forloopprogram.vb
@@ -0,0 +1,30 @@
+Module Module1
+
+    Sub Main()
+
+        Dim x As Integer = 0
+        Dim y As Integer = 0
+
+        For x = 1 To 10
+
+            For y = 1 To x
+                Console.Write(y)
+            Next
+
+            Console.WriteLine()
+
+
+        Next
+
+
+        Console.ReadLine()
+
+
+
+
+
+
+
+    End Sub
+
+End Module
